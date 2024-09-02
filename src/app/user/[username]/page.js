527ec78306816edd4be0a49d247ab6369a214ed4
Prateek Santani <prateeksantani98@gmail.com>
2024-09-02 13:05:47 +0530

    Routing

---
+++ src/app/user/[username]/page.js
@@ -0,0 +1,5 @@
+const DynamicPage = () => {
+  return <h1>Dynamic Page</h1>
+}
+
+export default DynamicPage;
---
+++ src/app/user/prateek/page.js
@@ -0,0 +1,7 @@
+export default function Prateek () {
+  return (
+    <div>
+      <h1>Prateek</h1>
+    </div>
+  );
+}
